1886431e0e9b3daeba355534af55c787ea0e582a
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2022-04-03 21:01:16 -0700

    fixed state machine, but sensor does not seems to act well

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,17 @@
 
 const size_t k8sSleepCycles = 3;
 const uint32_t kAnimIntervalMs = 150;
-const uint32_t kDigitIntervalMs = 60l * 1000l;  // int is 16 bit only on the ATmega328p so
+const uint32_t kDigitIntervalMs = 10l * 1000l;  // int is 16 bit only on the ATmega328p so
                                                 // we need to qualify with long to do the math!
 const uint32_t kBbarIntervalMs = kDigitIntervalMs / kBottomBarLen;
-const uint32_t kPirStabilizationMs = 8l * 1000l;
-const uint32_t kPeriodBeforeSleepMs = kPirStabilizationMs + 20l * 1000l;
+const uint32_t kPirStabilizationMs = 4l * 1000l;
+const uint32_t kPeriodBeforeSleepMs = kPirStabilizationMs + 8l * 1000l;
+
+const uint32_t kMotionDebounceCount = 3;
 
 const uint8_t kBuiltinLed = 13;
 const uint8_t kPirPowerPin = 11;
-const uint8_t kPirOutputPin = 10;
+const uint8_t kPirOutputPin = 5;
 
 Display gDisplay(kAnimIntervalMs, kBbarIntervalMs, kDigitIntervalMs);
 
@@ -20,37 +22,42 @@ void StartPir() {
   // Provide power to the PIR (<1ma)
   digitalWrite(kPirPowerPin, HIGH);
 
-  const uint32_t cycles = 5;
+  const uint32_t cycles = 6;
   const uint32_t on_period = 25;
   const uint32_t off_period = (kPirStabilizationMs - cycles * on_period) / cycles;
   // Wait enough to get a stable reading
   for (uint8_t c = 0; c < cycles; c++) {
-    digitalWrite(kBuiltinLed, HIGH);
+    delay(off_period);
+    // digitalWrite(kBuiltinLed, HIGH);
+    // mess up the sensing?
     delay(on_period);
     digitalWrite(kBuiltinLed, LOW);
-    delay(off_period);
   }
-  // Serial.println("PIR ON");
+  Serial.println("PIR ON");
 }
 
 void StopPir() { digitalWrite(kPirPowerPin, LOW); }
 
 enum class State { STANDBY, WAITING, ACTIVE, FROZEN };
 
-State UpdateState(State state, bool motion, uint32_t start) {
+State UpdateState(State state, bool motion, uint32_t start, uint32_t now) {
   static uint32_t last_motion = 0;
+  static uint32_t motion_counter = 0;
 
-  uint32_t now = millis();
   switch (state) {
     case State::STANDBY:
       state = State::WAITING;
       break;
     case State::WAITING:
       if (motion) {
-        gDisplay.Start();
-        state = State::ACTIVE;
+        motion_counter++;
+        if (motion_counter > kMotionDebounceCount) {
+          gDisplay.Start();
+          state = State::ACTIVE;
+        }
       } else {
-        if ((now - start) > kPirStabilizationMs) {
+        motion_counter = 0;
+        if ((now - start) > kPirStabilizationMs*2) {
           state = State::STANDBY;
         }
       }
@@ -59,6 +66,7 @@ State UpdateState(State state, bool motion, uint32_t start) {
       if (motion) {
         last_motion = now;
       } else {
+        motion_counter = 0;
         if ((now - last_motion) > kPirStabilizationMs) {
           state = State::FROZEN;
         }
@@ -66,8 +74,12 @@ State UpdateState(State state, bool motion, uint32_t start) {
       break;
     case State::FROZEN:
       if (motion) {
-        state = State::ACTIVE;
+        motion_counter++;
+        if (motion_counter > kMotionDebounceCount) {
+          state = State::ACTIVE;
+        }
       } else {
+        motion_counter = 0;
         if ((now - last_motion) > kPeriodBeforeSleepMs) {
           state = State::STANDBY;
           gDisplay.Stop();
@@ -83,7 +95,7 @@ State UpdateState(State state, bool motion, uint32_t start) {
 
 void Configure() {
   pinMode(kPirPowerPin, OUTPUT);
-  pinMode(kPirOutputPin, INPUT_PULLUP);
+  pinMode(kPirOutputPin, INPUT);
 }
 
 void setup() {
@@ -97,34 +109,49 @@ void loop() {
   delay(250);
   digitalWrite(kBuiltinLed, LOW);
 
-  // Serial.begin(9600);
-  // Serial.println(counter);
+  Serial.begin(9600);
+  Serial.println(counter);
 
   if (counter % k8sSleepCycles == 0) {
+    Serial.println("START");
     uint32_t last_wakeup = 0;
     State state = State::STANDBY;
     Configure();
     // Only check movement every n 8s cycles
     StartPir();
     last_wakeup = millis();
+    uint8_t last_input = 0;
     while (true) {
       uint8_t input = digitalRead(kPirOutputPin);
-      state = UpdateState(state, (HIGH == input), last_wakeup);
+      uint32_t now = millis();
+      if (last_input != input) {
+        Serial.print("input=");
+        Serial.println(input);
+        last_input = input;
+      }
+      State last_state = state;
+      state = UpdateState(state, (HIGH == input), last_wakeup, now);
+      if (state != last_state) {
+        Serial.print("new state=");
+        Serial.println((uint8_t)state);
+      }
       if (state == State::ACTIVE) {
-        gDisplay.Update(last_wakeup);
+        gDisplay.Update(last_wakeup, now);
       } else {
         if (state == State::STANDBY) {
+          // if ((now - last_wakeup) > 2000) {
           break;
+          // }
         }
       }
-      delay(10);  // no real reason but to not hammer the display?
+      delay(50);  // no real reason but to not hammer the display?
     }
     StopPir();
   }  // skip wakeup periods
 
-  // Serial.println("SLEEP");
-  // Serial.end();
+  Serial.println("SLEEP");
+  Serial.end();
   // Go to sleep
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_ON);
+  LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_ON);
   counter++;
 }
